a4592f14a27216fed015c77ff8b6610739f9cc5c
Howard Payne <paynehd@gmail.com>
2018-12-07 05:13:41 -0500

    updated from work

--- src/calc.py
+++ src/calc.py
@@ -1,21 +1,20 @@
-from .token import Token
-
-
+'''parser'''
+from src.token import Token
 
 class Interpreter(object):
+    '''Parser'''
 
     def __init__(self, text):
-        # client string input, e.g. "3 + 5", "12 - 5", etc
-        self.text = text
-        # self.pos is an index into self.text
-        self.pos = 0
-        # current token instance
-        self.current_token = None
+        self.text = text            # client string input, e.g. "3 + 5", "12 - 5", etc
+        self.pos = 0                # self.pos is an index into self.text
+        self.current_token = None   # current token instance
         self.current_char = self.text[self.pos]
 
 
-    def error(self):
-        raise Exception('Error parsing input')
+    # LEX ***********************
+
+    def error(self, msg):
+        raise Exception('Error parsing input : ' + msg)
 
 
     def advance(self):
@@ -28,6 +27,7 @@ class Interpreter(object):
 
 
     def skip_whitespace(self):
+        ''' [ \t\n]* '''
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
@@ -42,7 +42,8 @@ class Interpreter(object):
 
 
     def get_next_token(self):
-        """Lexical analyzer (also known as scanner or tokenizer)
+        """
+        Lexical analyzer (also known as scanner or tokenizer)
         This method is responsible for breaking a sentence
         apart into tokens.
         """
@@ -63,48 +64,133 @@ class Interpreter(object):
                 self.advance()
                 return Token(Token.MINUS, '-')
 
-            self.error()
+            if self.current_char == '*':
+                self.advance()
+                return Token(Token.MULTIPLY, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(Token.DIVIDE, '/')
+
+            self.error('get_next_token - unknown char')
 
         return Token(Token.EOF, None)
 
 
-    def eat(self, token_type):
-        # compare the current token type with the passed token type and
-        # if they match then "eat" the current token and assign the next
-        # token to the self.current_token, otherwise raise an exception.
+    def match(self, token_type):
+        ''' 
+        compare the current token type with the passed token type and
+        if they match then "eat" the current token and assign the next
+        token to the self.current_token, otherwise raise an exception.
+        '''
         if self.current_token.type == token_type:
             self.current_token = self.get_next_token()
         else:
-            self.error()
+            self.error('Unknown toke type : ' + token_type)
 
+    # def term(self):
+    #     ''' A TERM is + | - '''
+    #     token = self.current_token
+    #     self.match(Token.INTEGER)
+    #     return token.value
+
+    
+    def term(self ):
+        ''' A TERM -> + | - '''
+
+        token = self.current_token
+        self.factor()
+
+        while (True):
+
+            if (self.current_token.type in ['*','/','DIV','MOD']):
+                t = self.get_next_token()
+                self.match(t)
+                self.factor()
+                #emit(t, Token.NONE)
+                return token.value
+
+            else:
+                return token.value
+
+
+    def factor(self):
+        ''' A Factory -> ( expr ) | NUM | ID '''
+        if (self.current_token.type == Token.LPAREN == "("):
+            self.match("(") 
+            self.expr()
+            self.match(")")
+            return token.value
+
+        elif (self.current_token.type == Token.NUM):
+            #emit(Token.NUM, Token.token_value) ; 
+            self.match(Token.NUM)
+            return token.value
+
+        elif (self.current_token.type == Token.ID):
+            #emit(Token.ID, Token.token_value); 
+            self.match(Token.ID)
+            return token.value
+        else:
+            self.error("factor syntax error")
 
     def expr(self):
         """Parser / Interpreter
-        expr -> INTEGER PLUS INTEGER
-        expr -> INTEGER MINUS INTEGER
+        EXPR -> [TERM OP TERM]+ 
+        TERM -> [INTEGER]+
+        INTEGER -> [0..9] 
+        OP -> PLUS | MINUS | MULTIPLY | DIVIDE
         """
-        # set current token to the first token taken from the input
-        self.current_token = self.get_next_token()
+        
+        self.current_token = self.get_next_token() # set current token to the first token taken from the input
+        result = self.term()
 
+        while self.current_token.type in (Token.PLUS, Token.MINUS):
+            token = self.current_token
+            if (token.type == Token.PLUS):
+                self.match(Token.PLUS)
+                result = result + self.term()
+            elif (token.type == Token.MINUS):
+                self.match(Token.MINUS)
+                result = result - self.term()
+        
+        return result
+
+    # def expr(self):
+    
+    #     self.term()
+        
+    #     while(True):
+
+    #         if (parser.lookahead == '+' or parser.lookahead == '-'):
+    #             t = parser.lookahead
+    #             self.match(t); 
+    #             self.term(); 
+    #             emit(t, Token.NONE)
+
+    #         else:
+    #             return
+    
+        '''
         # we expect the current token to be an integer
         left = self.current_token
-        self.eat(Token.INTEGER)
-
+        self.match(Token.INTEGER)
+        
         # we expect the current token to be either a '+' or '-'
         op = self.current_token
 
         if op.type == Token.PLUS:
-            self.eat(Token.PLUS)
+            self.match(Token.PLUS)
         elif op.type == Token.MINUS:
-            self.eat(Token.MINUS)
+            self.match(Token.MINUS)
         elif op.type == Token.MULTIPLY:
-            self.eat(Token.MULTIPLY)
+            self.match(Token.MULTIPLY)
         elif op.type == Token.DIVIDE:
-            self.eat(Token.DIVIDE)
+            self.match(Token.DIVIDE)
 
         # we expect the current token to be an integer
         right = self.current_token
-        self.eat(Token.INTEGER)
+        self.match(Token.INTEGER)
         # after the above call the self.current_token is set to EOF token
 
         # at this point either the INTEGER PLUS INTEGER or the INTEGER MINUS 
@@ -113,13 +199,17 @@ class Interpreter(object):
         # effectively interpreting client input
         if op.type == Token.PLUS:
             result = left.value + right.value
-        else:
+        elif op.type == Token.MINUS:   
             result = left.value - right.value
+        elif op.type == Token.MULTIPLY:   
+            result = left.value * right.value
+        elif op.type == Token.DIVIDE:   
+            result = left.value / right.value
         return result
+        '''
 
 
-
-def main():
+def runmain():
     while True:
         try:
             text = input('calc> ')
@@ -133,4 +223,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    runmain()
